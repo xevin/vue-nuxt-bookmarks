5915ad1135c0a45288b118c49f9a1e7f39e598b6
Степан Крапивин <xevinbox@yandex.ru>
2020-09-22 15:09:45 +0300

    fixed cover names

--- assets/books.js
+++ assets/books.js
@@ -3,77 +3,77 @@ export default {
     {
       id: 1,
       title: "Жук в муравейнике",
-      cover: "book01.png",
+      cover: "book01.jpg",
       price: 500,
       category: "Фантастика"
     },
     {
       id: 2,
       title: "Автостопом по галактике",
-      cover: "book02.png",
+      cover: "book02.jpg",
       price: 700,
       category: "Фантастика"
     },
     {
       id: 3,
       title: "Лабиринт отражений",
-      cover: "book03.png",
+      cover: "book03.jpg",
       price: 280,
       category: "Фантастика"
     },
     {
       id: 4,
       title: "Маятник Фуко",
-      cover: "book04.png",
+      cover: "book04.jpg",
       price: 900,
       category: "Мистика"
     },
     {
       id: 5,
       title: "Хроники заводной птицы",
-      cover: "book05.png",
+      cover: "book05.jpg",
       price: 700,
       category: "Мистика"
     },
     {
       id: 6,
       title: "Зелёная миля",
-      cover: "book06.png",
+      cover: "book06.jpg",
       price: 700,
       category: "Фантастика"
     },
     {
       id: 7,
       title: "Приключения Шерлока Холмса",
-      cover: "book07.png",
+      cover: "book07.jpg",
       price: 420,
       category: "Детектив"
     },
     {
       id: 8,
       title: "Убийство в «Восточном Экспрессе»",
-      cover: "book08.png",
+      cover: "book08.jpg",
       price: 500,
       category: "Детектив"
     },
     {
       id: 9,
       title: "Десять негритят",
-      cover: "book09.png",
+      cover: "book09.jpg",
       price: 390,
       category: "Детектив"
     },
     {
       id: 10,
       title: "Дагон",
-      cover: "book10.png",
+      cover: "book10.jpg",
       price: 300,
       category: "Ужасы"
     },
     {
       id: 11,
       title: "Зов Ктулху",
-      cover: "book11.png",
+      cover: "book11.jpg",
       price: 520,
       category: "Ужасы"
     },
